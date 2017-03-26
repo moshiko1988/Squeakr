97612cdcab52e3b5eb95505ea6721a5b6cdf6941
moshiko1988 <moshiko1988@gmail.com>
2017-03-26 16:54:36 -0400

    Follow deployment instructions

--- config/environment.js
+++ config/environment.js
@@ -3,10 +3,11 @@
 module.exports = function (environment) {
   'use strict';
   const ENV = {
-    modulePrefix: 'ga-wdi-boston.ember-auth',
+    modulePrefix: 'squeakr',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    apiHost: 'http://localhost:3000/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -49,7 +50,9 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/squeakr';
     ENV.locationType = 'hash';
+    ENV.apiHost = 'https://lit-island-32465.herokuapp.com/';
   }
 
   return ENV;
