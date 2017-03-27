b74bdfabc0673616890868344a6de387597d9a99
moshiko1988 <moshiko1988@gmail.com>
2017-03-27 16:36:51 -0400

    Add user privacy
    A user can only delete and edit his posts.

--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 title: DS.attr('string'),
 body: DS.attr('string'),
-user: DS.belongsTo('user')
+user: DS.belongsTo('user'),
+editable: DS.attr('boolean')
 });
